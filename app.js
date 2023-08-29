2facc6ce694a6d44337660f01d293a1ec33f6907
ahmedd-nawaz <ahmednawaz3926@gmail.com>
2023-08-29 23:37:20 +0500

    last commit

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
 function signup() {
+    var user_name = document.getElementById('user-name').value
     var email = document.getElementById('user-email').value
     var pass = document.getElementById('user-pass').value
+    localStorage.setItem('Username', user_name)
     localStorage.setItem('Email', email)
     localStorage.setItem('Password', pass)
-}
+}  
 
 function validateForm() {
-    var name = document.getElementById('user-name').value
+    var user_name = document.getElementById('user-name').value
     var email = document.getElementById('user-email').value
     var pass = document.getElementById('user-pass').value
-    if (email == "" || email == null, pass == "" || pass == null, name == "" || name == null) {
+    if (email == "" || email == null, pass == "" || pass == null, user_name == "" || user_name == null) {
       alert("Please fill out all fields");
       return false;
     }
-    location.href = './index.html'
+    location.href = './Index.html'
   }
 
-  
-
 function signin() {
-    var email = document.getElementById('user-email').value
+    var user_name = document.getElementById('user-name').value
+    var email = document.querySelector('input[name="user-email"]').value
     var pass = document.getElementById('user-pass').value
-    if (localStorage.getItem('Email') == email && localStorage.getItem('Password') == pass) {
+    if (localStorage.getItem('Username') == user_name && localStorage.getItem('Password') == pass) {
+        location.href = './welcome.html'
+    }
+    else if(localStorage.getItem('Email') == email){
         location.href = './welcome.html'
     }
     else {
-        alert('Please creat an account first...')
-        location.href = './Signup.html'
+        alert('Wrong credentials or sign up again')
     }
 }
-
 
 function signout() {
     localStorage.clear()
